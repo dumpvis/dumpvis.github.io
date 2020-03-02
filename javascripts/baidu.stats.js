01909646f1e6ed6b227bf36df14acfd602f6d74f
duanqz <duanqz@gmail.com>
2020-03-02 13:53:40 +0800

    Add baidu stats

---
+++ javascripts/baidu.stats.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?4b724b048e5c28759ffaa8c8f0e8d913";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
